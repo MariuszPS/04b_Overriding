5cf2bfde24047aac4932bf1e08ae41fcf74e3cb2
MariuszPS <k8kpol@gmail.com>
2022-04-13 11:19:41 +0200

    ok

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,6 +3,13 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+
+        Child child =new Child();
+
+       
+
+        output(child.sayHello());
+        output(child.sayHello("Hi"));
      
     }
 
